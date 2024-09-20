33f2aef420bfe61574c4f7a1b214b8be36fc3fe1
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-09-20 16:18:19 +0530

    gap change

--- src/components/SeatMap.jsx
+++ src/components/SeatMap.jsx
@@ -11,8 +11,8 @@ const SeatMap = ({ date, bookedSeats, setBookedSeats }) => {
   console.log("filtered",filteredBookedSeats)
 
   return (
-    <div className="flex items-center mx-3 mb-10 justify-center text-center pt-15">
-      <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-10 gap-4">
+    <div className="flex items-center ms-2 me-3 mb-10 justify-center text-center pt-15">
+      <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-10 gap-2 sm:gap-8">
         {seats.map((seat) => (
           <Seat
             key={seat.seatno}
--- src/components/SeminarHall.jsx
+++ src/components/SeminarHall.jsx
@@ -23,7 +23,7 @@ const SeminarHall = () => {
     
 
     return (
-        <div className='h-screen bg-green-200'>
+        <div className='h-screen'>
             <h1 className='text-center py-1  pt-12 text-2xl'>Seminar Hall Booking</h1>
             <h1 className='text-center py-1 text-xl'>Select Date</h1>
             <div className='flex justify-center items-center py-5 lg:py-14'>
